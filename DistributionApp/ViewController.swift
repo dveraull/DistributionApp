4d8ee1b92ae546a0bd0a2ac181caa4148385dfd4
Aceleradora Mobile Perú <imac1@imacdiana.local>
2018-01-30 15:57:22 -0500

    Primera versión del modelo de vistas

--- DistributionApp/ViewController.swift
+++ DistributionApp/ViewController.swift
@@ -13,8 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated);
+        navigationController?.setNavigationBarHidden(true , animated: false)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
